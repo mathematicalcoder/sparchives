947f89bd556d0082cb9b9ad847baf07e295064e0
mathemolympiad <kaiserchan1023@gmail.com>
2025-01-18 11:26:01 +0800

    CSS fixes

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,29 +1,28 @@
 // display navbar
 document.getElementById("navbar").innerHTML = `
-<nav class="navbar navbar-expand-sm navbar-dark">
+<nav class="navbar fixed-top navbar-expand-sm navbar-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">SPARChives</a>
+    <a class="navbar-brand" href="https://wdprojsrbattungchan.glitch.me/">SPARChives</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link disabled" href="examcalendar">Exam Calendar</a>
+        <a class="nav-link disabled" href="https://wdprojsrbattungchan.glitch.me/examcalendar">Exam Calendar</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="reviewers">Reviewers</a>
+        <a class="nav-link" href="https://wdprojsrbattungchan.glitch.me/reviewers">Reviewers</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="mocktests">Mock Tests</a>
+        <a class="nav-link" href="https://wdprojsrbattungchan.glitch.me/mocktests">Mock Tests</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="gradesheet">Gradesheet</a>
+        <a class="nav-link disabled" href="https://wdprojsrbattungchan.glitch.me/gradesheet">Gradesheet</a>
       </li>
     </ul>
   </div>
-</nav>
-`;
+</nav>`;
 
 // display footer
 document.getElementById("footer").innerHTML += `<p>SPARChives. &copy; 2024 PSHS-MC SPARC 2028</p>`;
