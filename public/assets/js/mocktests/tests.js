18c1bc464d41527ca522129c546acde08d843a9e
Kaiser Chan <b2028ktachan@pshs.edu.ph>
2025-01-30 14:17:21 +0800

    fix textarea, put placeholders for inputs

--- public/assets/js/mocktests/tests.js
+++ public/assets/js/mocktests/tests.js
@@ -106,9 +106,9 @@ function showQuestions(details, questions, hps) {
     // show input box, question header, and question as a table row
     var element = document.createElement("tr")
     if (questions[i].type == "essay") {
-      element.innerHTML = `<td colspan="2"><span class="mtHeading">Question ${i+1} (${questions[i].points} point/s).</span> ${questions[i].question}<br><br><span id="p${i+1}Status"><textarea id="p${i+1}Answer"></span></td>`
+      element.innerHTML = `<td colspan="2"><span class="mtHeading">Question ${i+1} (${questions[i].points} point/s).</span> ${questions[i].question}<br><br><span id="p${i+1}Status"><textarea id="p${i+1}Answer" placeholder="Enter answer"></textarea></span></td>`
     } else {
-      element.innerHTML = `<td width="200px" id="p${i+1}Status"><input type="${questions[i].type}" id="p${i+1}Answer"></td> <td><span class="mtHeading">Question ${i+1} (${questions[i].points} point/s).</span> ${questions[i].question}</td>`
+      element.innerHTML = `<td width="200px" id="p${i+1}Status"><input type="${questions[i].type}" id="p${i+1}Answer" placeholder="Enter answer"></td> <td><span class="mtHeading">Question ${i+1} (${questions[i].points} point/s).</span> ${questions[i].question}</td>`
     }
     document.getElementById("questions").appendChild(element)
   }
