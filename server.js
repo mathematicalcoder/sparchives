cd5319cb0475bfd347a4abb8a6358c3dfdcbb968
Kaiser Chan <b2028ktachan@pshs.edu.ph>
2025-03-17 15:31:27 +0800

    set up hbs and user portal (no login yet)

--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-// Step 3: Require/Loads the express module
 const express = require('express');
-// body-parser is used to read data payload from the http request body
-const bodyParser = require('body-parser'); 
-//  path is used to set default directories for MVC and also for the static files
-const path = require('path'); 
-// include the defined package
-
-
-// Step 4: Creates our express server
 const app = express();
-
-//Serves static files inside the public folder
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'hbs');
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//Sets a basic route index.hbs when website initially starts and when home is clicked from the nav bar or whenever a process needs to go back to home 
-app.get('/', (req, res) => {
-    res.render('index.hbs');
-})
-
-
-// Step 5: Start HTTP Server on a port number 3000
-// This will create a web service for your own project
 const port = 5000;
-app.listen(port, () => console.log(`App listening to port ${port}`));
+
+// Set the view engine to Handlebars
+app.set('view engine', 'hbs');
+
+// Serve static files from the "public" folder
+app.use(express.static('public'));
+
+// Middleware to parse URL-encoded bodies (form data)
+app.use(express.urlencoded({ extended: true }));
+
+// Define a route
+app.get('/portal', (req, res) => {
+    res.render('portalHome.hbs', {name: "Kaiser Chan", member: true, head: true});
+  });
+
+app.get('/portal/add/rev', (req, res) => {
+    res.render('revContrib.hbs');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
