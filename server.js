bd7b9882a1966ffa9f9789c67cfd5d8e889660c7
mathemolympiad <kaiserchan1023@gmail.com>
2025-03-18 21:26:27 +0800

    finished the member registration form among other things

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = 5000;
 
@@ -9,6 +11,7 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 // Middleware to parse URL-encoded bodies (form data)
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Define a route
@@ -18,6 +21,20 @@ app.get('/portal', (req, res) => {
 
 app.get('/portal/add/rev', (req, res) => {
     res.render('revContrib.hbs');
+});
+
+app.get('/portal/adminReg', (req, res) => {
+  res.render('adminMemberReg.hbs');
+});
+
+app.get('/portal/members', (req, res) => {
+  try {
+    const members = JSON.parse(fs.readFileSync(membersFilePath));
+    const memberList = Object.values(members);
+    res.render('memberList.hbs', {members: memberList});
+  } catch (error) {
+    res.status(500).send('Error loading member list!');
+  }
 });
 
 app.listen(port, () => {
@@ -37,17 +54,23 @@ if (!fs.existsSync(membersFilePath)) {
 
 // handle member registration
 app.post('/portal/adminReg/submit', (req, res) => {
-  const { name, section } = req.body;
+  const { surname, given, mi, section } = req.body;
+  const name = `${surname.toUpperCase()}, ${given} ${mi}.`;
   console.log(`Registering ${name} (${section}) as a member...`);
 
-  if (!name || !section) {
-    console.error("At least one required field is missing! ", { name, section });
+  if (!surname || !given || !mi || !section) {
+    console.error("At least one required field is missing! ", { surname, given, mi, section });
     return res.status(400).send("At least one required field is missing!")
   }
 
   try {
-    // WIP
+    const members = JSON.parse(fs.readFileSync(membersFilePath));
+    const key = `${surname.toUpperCase()}_${section}`;
+    members[key] = {surname, given, mi, section};
+    fs.writeFileSync(membersFilePath, JSON.stringify(members, null, 2));
+    res.redirect('/portal/members');
   } catch (error) {
-    // WIP
+    console.error(error);
+    res.status(500).send('Error registering the member!');
   }
 });
